0cf9883c3cfa08fd91f71dbef9127bceacb97282
Aqilulhaq <162259726+Aqilulhaq@users.noreply.github.com>
2024-03-19 10:10:33 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+def luas_segitiga():
+    a = int(input("Masukan alas segitiga: "))
+    t = int(input("Masukan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+luas_segitiga()
+
+def luas_persegi_panjang():
+    p = int(input("Masukan panjang persegi: "))
+    l = int(input("Masukan panjang lebar: "))
+    luas = p * l
+    print("Luas persegi adalah: ", luas) 
+
+luas_persegi_panjang()
